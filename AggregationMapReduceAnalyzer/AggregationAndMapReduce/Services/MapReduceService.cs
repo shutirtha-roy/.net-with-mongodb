ac669ae4867b74f9115cccfb65291b156525a506
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-10-22 03:09:28 +1100

    data receiveing implemented but with NaN in some attributes

--- AggregationMapReduceAnalyzer/AggregationAndMapReduce/Services/MapReduceService.cs
+++ AggregationMapReduceAnalyzer/AggregationAndMapReduce/Services/MapReduceService.cs
@@ -16,43 +16,52 @@ namespace AggregationAndMapReduce.Services
 
         public async Task<List<BsonDocument>> AnalyzeSales(DateTime startDate, DateTime endDate)
         {
-            var map = new BsonJavaScript(@"
-                function() {
-                    var year = this.date.getFullYear();
-                    var month = this.date.getMonth() + 1;
-                    var sales = this.price * this.quantity;
-                    emit({ year: year, month: month, category: this.category }, { totalSales: sales, count: 1 });
-                }");
+                var map = new BsonJavaScript(@"
+                    function() {
+                        var year = this.date.getFullYear();
+                        var month = this.date.getMonth() + 1;
+                        var sales = this.price * this.quantity;
+                        emit({ year: year, month: month, category: this.category }, { totalSales: sales, count: 1 });
+                    }");
 
-            var reduce = new BsonJavaScript(@"
-                function(key, values) {
-                    var result = { totalSales: 0, count: 0 };
-                    values.forEach(function(value) {
-                        result.totalSales += value.totalSales;
-                        result.count += value.count;
-                    });
-                    return result;
-                }");
+                var reduce = new BsonJavaScript(@"
+                    function(key, values) {
+                        var result = { totalSales: 0, count: 0 };
+                        values.forEach(function(value) {
+                            result.totalSales += value.totalSales;
+                            result.count += value.count;
+                        });
+                        return result;
+                    }");
 
-            var finalize = new BsonJavaScript(@"
-                function(key, reducedValue) {
-                    reducedValue.averageSale = reducedValue.totalSales / reducedValue.count;
-                    return reducedValue;
-                }");
+                var finalize = new BsonJavaScript(@"
+                    function(key, reducedValue) {
+                        reducedValue.averageSale = reducedValue.totalSales / reducedValue.count;
+                        return reducedValue;
+                    }");
 
-            var options = new MapReduceOptions<BsonDocument, BsonDocument>
-            {
-                OutputOptions = MapReduceOutputOptions.Inline,
-                Filter = Builders<BsonDocument>.Filter.And(
-                    Builders<BsonDocument>.Filter.Gte("date", startDate),
-                    Builders<BsonDocument>.Filter.Lt("date", endDate)
-                ),
-                Finalize = finalize,
-                Sort = Builders<BsonDocument>.Sort.Ascending("date")
-            };
+                var options = new MapReduceOptions<BsonDocument, BsonDocument>
+                {
+                    OutputOptions = MapReduceOutputOptions.Inline,
+                    Filter = Builders<BsonDocument>.Filter.And(
+                        Builders<BsonDocument>.Filter.Gte("date", startDate),
+                        Builders<BsonDocument>.Filter.Lt("date", endDate)
+                    ),
+                    Finalize = finalize,
+                    Sort = Builders<BsonDocument>.Sort.Ascending("date")
+                };
 
-            var results = await _collection.MapReduceAsync(map, reduce, options);
-            return results.ToList();
+
+                var results = await _collection.MapReduceAsync(map, reduce, options);
+
+                if (results == null)
+                {
+                    return new List<BsonDocument>();
+                }
+
+                var resultList = await results.ToListAsync();
+
+                return resultList;
         }
     }
 }
