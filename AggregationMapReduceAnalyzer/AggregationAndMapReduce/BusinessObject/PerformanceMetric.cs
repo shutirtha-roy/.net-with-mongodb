fa69022747ca3f75ba7cf48874c93b7a9deabce0
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-10-26 17:00:59 +1100

    graph implemented

---
+++ AggregationMapReduceAnalyzer/AggregationAndMapReduce/BusinessObject/PerformanceMetric.cs
@@ -0,0 +1,13 @@
+﻿namespace AggregationAndMapReduce.BusinessObject
+{
+    public class PerformanceMetric
+    {
+        public int Documents { get; set; }
+        public double AggregationTime { get; set; }
+        public double MapReduceTime { get; set; }
+        public double AggregationMemory { get; set; }
+        public double MapReduceMemory { get; set; }
+        public double AggregationPWS { get; set; }
+        public double MapReducePWS { get; set; }
+    }
+}
--- AggregationMapReduceAnalyzer/AggregationAndMapReduce/Services/PerformanceComparisonService.cs
+++ AggregationMapReduceAnalyzer/AggregationAndMapReduce/Services/PerformanceComparisonService.cs
@@ -2,7 +2,9 @@
 using Amazon.Runtime;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using ScottPlot;
 using System.Diagnostics;
+using ScottPlot;
 
 namespace AggregationAndMapReduce.Services
 {
@@ -14,6 +16,7 @@ namespace AggregationAndMapReduce.Services
         private readonly IAggregationService _aggregationAnalyzer;
         private readonly IMapReduceService _mapReduceAnalyzer;
         private readonly List<string> _metrics = new List<string>();
+        private readonly IList<PerformanceMetric> _performanceMetricList;
 
         public PerformanceComparisonService(IDataGenerationService dataGenerator,
             IAggregationService aggregationAnalyzer, IMapReduceService mapReduceService)
@@ -24,6 +27,7 @@ namespace AggregationAndMapReduce.Services
             _dataGenerator = dataGenerator;
             _aggregationAnalyzer = aggregationAnalyzer;
             _mapReduceAnalyzer = mapReduceService;
+            _performanceMetricList = new List<PerformanceMetric>();
         }
 
         public async Task<List<string>> RunComparison(int[] dataSizes)
@@ -50,9 +54,12 @@ namespace AggregationAndMapReduce.Services
                 _metrics.Add($"Map-Reduce: {mapReduceResult}");
                 _metrics.Add($"-----------------------------");
 
+                await CreateMetric(size, aggregationResult, mapReduceResult);
 
                 await _database.DropCollectionAsync(_collectionName);
             }
+
+            await CreateGraph();
 
             return _metrics;
         }
@@ -80,6 +87,79 @@ namespace AggregationAndMapReduce.Services
                 PeakWorkingSet = processBefore.PeakWorkingSet64 
                     / (1024.0 * 1024.0),
             };
+        }
+
+        public async Task CreateGraph()
+        {
+            var xValues = _performanceMetricList.Select(m => (double)m.Documents).ToArray();
+
+            // Generate Execution Time Chart
+            var timePlot = new ScottPlot.Plot();
+
+            var aggTime = timePlot.Add.Scatter(xValues, _performanceMetricList.Select(m => m.AggregationTime).ToArray());
+            aggTime.LegendText = "Aggregation Pipeline";
+
+            var mrTime = timePlot.Add.Scatter(xValues, _performanceMetricList.Select(m => m.MapReduceTime).ToArray());
+            mrTime.LegendText = "Map-Reduce";
+
+            timePlot.Title("Execution Time Comparison");
+            timePlot.XLabel("Number of Documents");
+            timePlot.YLabel("Execution Time (seconds)");
+            timePlot.ShowLegend();
+
+            var memoryPlot = new ScottPlot.Plot();
+
+            var aggMemory = memoryPlot.Add.Scatter(xValues, _performanceMetricList.Select(m => m.AggregationMemory).ToArray());
+            aggMemory.LegendText = "Aggregation Pipeline";
+
+            var mrMemory = memoryPlot.Add.Scatter(xValues, _performanceMetricList.Select(m => m.MapReduceMemory).ToArray());
+            mrMemory.LegendText = "Map-Reduce";
+
+            memoryPlot.Title("Memory Usage Comparison");
+            memoryPlot.XLabel("Number of Documents");
+            memoryPlot.YLabel("Memory Used (MB)");
+            memoryPlot.ShowLegend();
+
+            var pwsPlot = new ScottPlot.Plot();
+
+            var aggPWS = pwsPlot.Add.Scatter(xValues, _performanceMetricList.Select(m => m.AggregationPWS).ToArray());
+            aggPWS.LegendText = "Aggregation Pipeline";
+
+            var mrPWS = pwsPlot.Add.Scatter(xValues, _performanceMetricList.Select(m => m.MapReducePWS).ToArray());
+            mrPWS.LegendText = "Map-Reduce";
+
+            pwsPlot.Title("Peak Working Set Comparison");
+            pwsPlot.XLabel("Number of Documents");
+            pwsPlot.YLabel("Peak Working Set (MB)");
+            pwsPlot.ShowLegend();
+
+            var timePath = Path.Combine("result", "time.png");
+            timePlot.SavePng(timePath, 800, 600);
+
+            var memoryPath = Path.Combine("result", "memory.png");
+            memoryPlot.SavePng(memoryPath, 800, 600);
+
+            var pwsPath = Path.Combine("result", "pws.png");
+            pwsPlot.SavePng(pwsPath, 800, 600);
+        }
+
+        private async Task SaveGraph()
+        {
+
+        }
+
+        private async Task CreateMetric(int size, PerformanceResult aggregationResult, PerformanceResult mapReduceResult)
+        {
+            _performanceMetricList.Add(
+                new() { 
+                    Documents = size, 
+                    AggregationTime = aggregationResult.ExecutionTime.TotalSeconds,
+                    MapReduceTime = mapReduceResult.ExecutionTime.TotalSeconds,
+                    AggregationMemory = aggregationResult.MemoryUsed,
+                    MapReduceMemory = mapReduceResult.MemoryUsed,
+                    AggregationPWS = aggregationResult.PeakWorkingSet,
+                    MapReducePWS = mapReduceResult.PeakWorkingSet
+                });
         }
     }
 }
